6f4bddcb90afe1be49b24348658150fc6a98a4e3
jz <jz@192.168.219.103>
2022-05-29 17:46:12 +0900

    commit test

--- src/main/java/com/gloryDuck/marketTree/model/vo/MemberEntity.java
+++ src/main/java/com/gloryDuck/marketTree/model/vo/MemberEntity.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public @Data class MemberEntity implements Serializable {
-	 
+	  
 	private static final long serialVersionUID = 1L;
 	
 	@Id
